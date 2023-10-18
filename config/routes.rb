c1b81cc42e6b38dcfec5e09e98f752689fa7092b
shimizuseiya <sxs.irohasu@gmail.com>
2023-10-19 00:58:25 +0900

    プライバシーポリシーページと利用規約ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
+  get  '/privacy_policy', to: 'static_pages#privacy_policy'
+  get  '/terms_of_service', to: 'static_pages#terms_of_service'
 end
