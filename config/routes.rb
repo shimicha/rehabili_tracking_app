b9780210912a2001d73b59769ff1d2c36e1a6207
shimizuseiya <sxs.irohasu@gmail.com>
2023-10-21 18:51:48 +0900

    パスワードは忘れた方はこちらページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  resources :password_resets, only: %i[new create edit update]
+  get 'password_resets/new'
+  get 'password_resets/create'
+  get 'password_resets/edit'
+  get 'password_resets/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
   get  'privacy_policy', to: 'static_pages#privacy_policy'
