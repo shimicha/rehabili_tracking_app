22997747c461a24c10b46cb40fe3615fdcb302ab
shimizuseiya <sxs.irohasu@gmail.com>
2023-10-22 00:45:31 +0900

    ログイン機能と新規作成機能実装

--- db/migrate/20231019114950_sorcery_core.rb
+++ db/migrate/20231019114950_sorcery_core.rb
@@ -2,7 +2,7 @@ class SorceryCore < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :email,            null: false, index: { unique: true }
-      t.string :password_digest
+      t.string :crypted_password
       t.string :salt
       t.string :name
       t.timestamps                null: false
