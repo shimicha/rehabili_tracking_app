25bc1012489338fca9094bc0d47fd638a68a5ed4
shimizuseiya <sxs.irohasu@gmail.com>
2023-10-23 00:27:39 +0900

    パスワード変更実装

---
+++ test/controllers/passwords_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PasswordsControllerTest < ActionDispatch::IntegrationTest
+  test "should get edit" do
+    get passwords_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get passwords_update_url
+    assert_response :success
+  end
+
+end
