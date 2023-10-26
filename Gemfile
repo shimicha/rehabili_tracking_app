ca5e84af5bb09ddaa86ebc56e5131c67a827a013
shimizuseiya <sxs.irohasu@gmail.com>
2023-10-26 20:54:26 +0900

    パスワードリセット機能実装

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'letter_opener_web'
 end
 
 group :test do
@@ -65,4 +66,3 @@ gem 'config'
 gem 'sorcery'
 gem 'font-awesome-sass', '~> 5.12.0'
 gem 'jquery-rails'
-gem 'letter_opener_web'
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,12 @@ ActiveRecord::Schema.define(version: 2023_10_21_085850) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
