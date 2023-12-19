de0d09e42cf4dceec32cd4abdff672094c4eddcb
shimizuseiya <sxs.irohasu@gmail.com>
2023-12-19 21:56:23 +0900

    FullCalendarブラウザ表示

--- app/controllers/exercise_progresses_controller.rb
+++ app/controllers/exercise_progresses_controller.rb
@@ -1,5 +1,6 @@
 class ExerciseProgressesController < ApplicationController
 def index
+    @exercise_progresses = ExerciseProgresse.all
 end
 
 def new
@@ -7,5 +8,23 @@ def new
     @exercise_progresse = ExerciseProgresse.new
     @exercise_progresse.date = Date.today
 end
+
+def create
+    @exercise_progresse = ExerciseProgresse.new(exercise_progresse_params)
+    
+    @exercise_progresse.tag = params[:exercise_progresse][:tag]
+  
+    if @exercise_progresse.save
+      redirect_to @exercise_progresse, notice: 'Exercise Progresse was successfully created.'
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def exercise_progresse_params
+    params.require(:exercise_progresse).permit(:date, tag: [], exercise_plans_attributes: [:training_menu, :progresse_keep])
+  end
 
 end
--- app/models/exercise_progresse.rb
+++ app/models/exercise_progresse.rb
@@ -1,4 +1,5 @@
 class ExerciseProgresse < ApplicationRecord
     belongs_to :user 
+    
 
 end
