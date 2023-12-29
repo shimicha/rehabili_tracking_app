f499f166f53a995d4113557e45f8d6003eb4e05d
shimizuseiya <sxs.irohasu@gmail.com>
2023-12-29 11:43:23 +0900

    ダッシュボードから各月のカレンダーページに画面遷移できるように実装

--- app/assets/javascripts/exercise_progresses.js
+++ app/assets/javascripts/exercise_progresses.js
@@ -1,7 +1,22 @@
 $(document).on('turbolinks:load', function () {
     if ($('#calendar').length) {
+
+        var urlParams = new URLSearchParams(window.location.search);
+        var monthParam = urlParams.get('month');
+
+        // 英語の月名を月のインデックスに変換するマッピング
+        var monthNames = {
+            "January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6,
+            "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12
+        };
+
+        var currentYear = new Date().getFullYear();
+        var monthIndex = monthParam ? monthNames[monthParam] : new Date().getMonth();
+        var defaultDate = new Date(currentYear, monthIndex, 1);
+
         // カレンダーを初期化
         var calendar = $('#calendar').fullCalendar({
+            defaultDate: defaultDate.toISOString().slice(0, 10),
             events: '/exercise_progresses.json', // exercise_progresseモデルに対応したパスを指定
             // カレンダー上部を年月で表示させる
             titleFormat: 'YYYY年 M月',
@@ -42,6 +57,8 @@ $(document).on('turbolinks:load', function () {
                 element.css("padding", "5px");
             }
         });
+
+        
 
         // カレンダーが破棄される前に処理を実行
         $(document).on('turbolinks:before-cache', function() {
