805ea781984044b47a5802ffa0b3fc0ef535c553
shimizuseiya <sxs.irohasu@gmail.com>
2023-12-30 23:30:09 +0900

    ダッシュボードの最終更新日表示実装

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,13 @@
 class DashboardsController < ApplicationController
     def index
-        @monthly_data = (1..12).map do |month|
-          {
-            month: Date::MONTHNAMES[month],
-            details: "デフォルトの詳細"
-          }
-        end
+      @monthly_data = (1..12).map do |month|
+        last_day_of_month = Date.new(Date.today.year, month, -1)
+        latest_progress = ExerciseProgresse.where('date <= ?', last_day_of_month)
+                                          .order(updated_at: :desc).first
+        {
+          month: Date::MONTHNAMES[month],
+          latest_progress: latest_progress
+        }
       end
+    end
 end
