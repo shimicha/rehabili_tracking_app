e4519812db762a9f35814c52e9f8c32955ec0a26
shimizuseiya <sxs.irohasu@gmail.com>
2024-01-12 00:45:57 +0900

    LINE API周囲の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     skip_before_action :require_login
     
+    
     def new
         @user = User.new
     end
@@ -24,4 +25,5 @@ class UsersController < ApplicationController
       def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
       end
+
 end
