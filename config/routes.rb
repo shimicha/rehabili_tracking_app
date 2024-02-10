835477ef32550b6734143bf3cdf4d85b678f612f
shimizuseiya <sxs.irohasu@gmail.com>
2024-02-10 18:04:56 +0900

    カレンダーに祝日を表示させようとしている

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,6 @@ Rails.application.routes.draw do
   resources :tops, only: [:new]
   resources :profiles
 
-  resources :posts
-
-  resources :dashboards, only: %i[index]
   resources :exercise_progresses, only: %i[index new create show]
 
   post '/line_bot/callback', to: 'line_bot#callback'
@@ -32,7 +29,7 @@ Rails.application.routes.draw do
     delete 'logout', to: 'user_sessions#destroy'
     resources :exercise_plans, only: %i[index new create edit destroy update]
     resources :users, only: %i[index destroy edit update]
-    resources :profiles, only: %i[index update edit new create]
+    resources :profiles, only: %i[index update edit new create destroy]
     resources :exercise_progress_comments, only: %i[new create index]
   end
 end
